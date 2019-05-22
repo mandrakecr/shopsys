32d4513423aa3ae5d1c54e2e8319b693fd2b2b07
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2019-05-22 15:03:18 +0200

    added HeaderLinks object that is responsible for pagination links

--- packages/api/src/Controller/V1/ProductController.php
+++ packages/api/src/Controller/V1/ProductController.php
@@ -5,7 +5,10 @@ namespace Shopsys\ApiBundle\Controller\V1;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 
 use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
+use FOS\RestBundle\Request\ParamFetcher;
 use FOS\RestBundle\View\View;
+use Shopsys\ApiBundle\Component\HeaderLinks\HeaderLinks;
 use Shopsys\FrameworkBundle\Model\Product\ProductFacade;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -38,5 +41,30 @@ class ProductController extends AbstractFOSRestController
         $product = $this->productFacade->getByUuid($uuid);
 
         return $this->handleView(View::create($product, 200));
+    }
+
+    /**
+     * Retrieves an multiple Product resources
+     * @Get("/products")
+     * @QueryParam(name="page", requirements="\d+", default=1)
+     * @QueryParam(name="uuids", map=true, requirements="[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}", allowBlank=false)
+     * @param \FOS\RestBundle\Request\ParamFetcher $paramFetcher
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function getProductsAction(ParamFetcher $paramFetcher): Response
+    {
+        $products = $this->productFacade->getById(1);
+
+        $page = (int)$paramFetcher->get('page');
+        $filterUuids = $paramFetcher->get('uuids');
+        $isFilterUuid = is_array($filterUuids); // if there are no uuids, $filterUuids === ''
+
+        $links = (new HeaderLinks())
+            ->add('http://localhost:8000/api/v1/products?page=2', 'next')
+            ->add('http://localhost:8000/api/v1/products?page=30', 'last');
+
+        $view = View::create($products, 200, ['Link', $links->format()]);
+
+        return $this->handleView($view);
     }
 }
